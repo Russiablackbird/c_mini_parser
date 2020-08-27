6b54a48090509c0ab67a192644fd5075f38a701b
Alexandr <russiablackbird@gmail.com>
2020-08-27 22:17:09 +0600

    Create c_parser.h

---
+++ Inc/c_parser.h
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+void *Parse(const char *str, const char *str2, const char *ob, const char *dest);
+
+#ifdef __cplusplus
+}
+#endif
