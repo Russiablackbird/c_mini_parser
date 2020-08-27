6877f625adea5ae025305ef55c4ee79abbf47b67
Alexandr <russiablackbird@gmail.com>
2020-08-27 22:18:14 +0600

    Create c_parser.c

---
+++ Src/c_parser.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdint.h>
+
+uint32_t Pos(const char *Substr, const char *S)
+{
+  return (uint32_t)(strstr(S, Substr) - S);
+}
+
+void *Parse(const char *str, const char *str2, const char *ob, const char *dest)
+{
+  uint32_t len = strlen(str);
+  return memcpy((uint32_t *)dest, (uint32_t *)((Pos(str, ob) + len) + (uint32_t)ob), Pos(str2, ob + (Pos(str, ob) + len)));
+}
